3ce6408434d39297640047a808c454e4dde5cccf
benwnuk <contact@benwnuk.com>
2014-02-04 22:30:51 -0800

    Grunt - final coffee and scss configuration

--- assets/scrolljunkie.js
+++ assets/scrolljunkie.js
@@ -1,4 +1,5 @@
 (function() {
+  test;
 
 
 }).call(this);
--- gruntfile.js
+++ gruntfile.js
@@ -2,25 +2,49 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
-	pkg : grunt.file.readJSON('package.json'),
-		coffee : {
-			options : {
+	pkg: grunt.file.readJSON('package.json'),
+		coffee: {
+			options: {
 				sourceMap: true
 			},
-			compile : {
-				files : {
+			compile: {
+				files: {
 					'assets/scrolljunkie.js' : 'source/scrolljunkie.coffee'
+				}
+			}
+		},
+		compass: {
+			dist:{
+				options: {
+					outputStyle: 'expanded',
+					debugInfo: true,
+					sassDir: 'source',
+					cssDir: 'assets'
+				}	
+			}			
+		},
+		watch: {
+			coffee: {
+				files: ['source/scrolljunkie.coffee'],
+				tasks: ['coffee'],
+				options: {
+					livereload: true
+				}
+			},
+			compass: {
+				files: ['source/scrolljunkie.scss'],
+				tasks: ['compass'],
+				options: {
+					livereload: true
 				}
 			}
 		}
 	});
 
 	// Load the plugin that provides the "uglify" task.
-	grunt.loadNpmTasks('grunt-concurrent');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-coffee');
-	
-	grunt.registerTask('default', ['coffee']);
+	grunt.registerTask('default', ['watch']);
 
 };
