00792887a4bb8b3f151a6c82e2016a733db28806
benwnuk <contact@benwnuk.com>
2014-02-14 01:05:50 -0800

    EOD

--- assets/scrolljunkie.js
+++ assets/scrolljunkie.js
@@ -1,6 +1,118 @@
 (function() {
   $(document).ready(function() {
-    return $.fn.scrollJunkie = function() {};
+    return $.fn.scrollJunkie = function(opts) {
+      var activeSelector, checkMediaQuery, computeOffset, debugOutput, i, io, log, newCollection, processEachEffect, sjBehaviors, sjCollection, v, vo;
+      activeSelector = '[data-scrolljunkie]';
+      debugOutput = true;
+      log = function(output) {
+        if (debugOutput) {
+          return console.log(output);
+        }
+      };
+      if (!$.data(document.body, 'sjBehaviors')) {
+        $.data(document.body, 'sjBehaviors', {});
+      }
+      sjBehaviors = $.data(document.body, 'sjBehaviors');
+      if ($.isPlainObject(opts)) {
+        for (io in opts) {
+          vo = opts[io];
+          sjBehaviors[io] = {
+            mediaQuery: vo.mediaQuery || '',
+            effects: (function() {
+              var _ref, _results;
+              _ref = vo.effects;
+              _results = [];
+              for (i in _ref) {
+                v = _ref[i];
+                _results.push({
+                  start: v.start || '-100vh',
+                  end: v.end || '100%',
+                  easing: v.easing || 'linear',
+                  init: v.init || function() {},
+                  resize: v.resize || function() {},
+                  perform: v.perform || function() {},
+                  transform: {
+                    scale: v.transform && v.transform.scale ? v.transform.scale : false,
+                    overflow: v.transform && v.transform.overflow ? v.transform.overflow : 'hide',
+                    align: v.transform && v.transform.align ? v.transform.align : 50,
+                    ratioX: v.transform && v.transform.ratioX ? v.transform.ratioX : 0,
+                    ratioY: v.transform && v.transform.ratioY ? v.transform.ratioY : 0
+                  }
+                });
+              }
+              return _results;
+            })()
+          };
+        }
+      }
+      newCollection = this.find(activeSelector);
+      if ($.data(document.body, 'sjCollection')) {
+        sjCollection = $.data(document.body, 'sjCollection').add(newCollection);
+      } else {
+        sjCollection = $.data(document.body, 'sjCollection', newCollection);
+      }
+      newCollection.each(function() {
+        var effect, elementBehavior, elementBehaviors, nb, that, _i, _j, _len, _len1, _ref, _ref1, _results;
+        elementBehaviors = [];
+        that = $(this);
+        $(this).data('elementBehaviors', elementBehaviors);
+        _ref = $(this).attr('data-scrolljunkie').split(',');
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          elementBehavior = _ref[_i];
+          if (sjBehaviors[elementBehavior] != null) {
+            nb = $.extend({}, sjBehaviors[elementBehavior]);
+            _ref1 = nb.effects;
+            for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+              effect = _ref1[_j];
+              effect.elements = {};
+              effect.host = that;
+              effect.data = {};
+            }
+            _results.push(elementBehaviors.push(nb));
+          } else {
+            _results.push(log("the behavior " + elementBehavior + " could not been initialized"));
+          }
+        }
+        return _results;
+      });
+      processEachEffect = function(callback) {
+        return sjCollection.each(function() {
+          var effect, elementBehavior, that, _i, _len, _ref, _results;
+          that = $(this);
+          _ref = $(this).data('elementBehaviors');
+          _results = [];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            elementBehavior = _ref[_i];
+            if ((callback != null) && checkMediaQuery(elementBehavior.mediaQuery)) {
+              _results.push((function() {
+                var _j, _len1, _ref1, _results1;
+                _ref1 = elementBehavior.effects;
+                _results1 = [];
+                for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+                  effect = _ref1[_j];
+                  _results1.push(callback.call(effect));
+                }
+                return _results1;
+              })());
+            } else {
+              _results.push(void 0);
+            }
+          }
+          return _results;
+        });
+      };
+      computeOffset = function(eh, vh, value) {};
+      checkMediaQuery = function(mq) {
+        return true;
+      };
+      if (debugOutput) {
+        window.sjBehaviors = sjBehaviors;
+        window.sjCollection = sjCollection;
+        window.computeOffset = computeOffset;
+        return window.processEachEffect = processEachEffect;
+      }
+    };
   });
 
 }).call(this);
--- source/scrollJunkie.coffee
+++ source/scrollJunkie.coffee
@@ -1,2 +1,81 @@
 $(document).ready ()->
-	$.fn.scrollJunkie = ()->
+	$.fn.scrollJunkie = (opts)->
+
+		activeSelector = '[data-scrolljunkie]'
+		debugOutput = true
+
+		log = (output)->
+			console.log(output) if debugOutput
+
+		$.data(document.body, 'sjBehaviors', {}) unless $.data(document.body, 'sjBehaviors') 
+		sjBehaviors = $.data(document.body, 'sjBehaviors')
+		if $.isPlainObject(opts)
+			for io, vo of opts
+				sjBehaviors[io] =
+					mediaQuery : vo.mediaQuery || ''
+					effects : for i, v of vo.effects
+						start : v.start || '-100vh'
+						end : v.end || '100%'
+						easing : v.easing || 'linear'
+						init : v.init || ()->
+						resize : v.resize || ()->
+						perform : v.perform || ()->
+						transform : 
+							scale : if v.transform and v.transform.scale then v.transform.scale else false
+							overflow : if v.transform and v.transform.overflow then v.transform.overflow else 'hide'
+							align : if v.transform and v.transform.align then v.transform.align else 50
+							ratioX : if v.transform and v.transform.ratioX then v.transform.ratioX else 0
+							ratioY : if v.transform and v.transform.ratioY then v.transform.ratioY else 0
+
+		newCollection = this.find(activeSelector)
+		if $.data(document.body, 'sjCollection') 
+			sjCollection = $.data(document.body, 'sjCollection').add(newCollection)
+		else
+			sjCollection = $.data(document.body, 'sjCollection', newCollection)
+		
+		# bind actual behaviors to the elements
+		#  and bind element-specific data
+		newCollection.each ()->
+			#log $(this)
+			elementBehaviors = []
+			that = $(this)
+			$(this).data 'elementBehaviors', elementBehaviors
+			for elementBehavior in $(this).attr('data-scrolljunkie').split(',')
+				if sjBehaviors[elementBehavior]?
+					nb = $.extend({}, sjBehaviors[elementBehavior])
+					for effect in nb.effects
+						effect.elements = {}
+						effect.host = that
+						effect.data = {}
+					elementBehaviors.push(nb)
+				else
+					log "the behavior #{elementBehavior} could not been initialized" 
+
+		processEachEffect = (callback)->
+			# provide a callback, will be passed the effect in the context of the element as this
+			sjCollection.each ()->
+				that = $(this)
+				for elementBehavior in $(this).data('elementBehaviors')
+					if callback? and checkMediaQuery(elementBehavior.mediaQuery)
+						for effect in elementBehavior.effects
+							# callback will consider the effect itself to be "this"
+							callback.call(effect)
+			
+		computeOffset = (eh, vh, value)->
+			# provided the element height, vieport height, and the value string, return an offset in px used to compute start/end
+			# strip out all non-acceptable chars
+			# replace vh
+			# replace %
+			# evaluate and return
+
+		checkMediaQuery = (mq)->
+			#check if the media query currently applies, return true or false
+			return true
+
+		if debugOutput
+			window.sjBehaviors = sjBehaviors
+			window.sjCollection = sjCollection
+			window.computeOffset = computeOffset
+			window.processEachEffect = processEachEffect
+
+		
